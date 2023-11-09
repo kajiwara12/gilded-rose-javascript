f758e31c1991961a99277c135b7f85a48288be0c
Quinn Kaji <quinnkaji@gmail.com>
2023-11-09 06:54:53 -1000

    fixed code tests are passing

--- gilded-rose.js
+++ gilded-rose.js
@@ -45,7 +45,7 @@ export class Basic extends Item {
   updateQuality() {
     this.sellIn--;
 
-    if (this.quality <= 0) this.quality = 0;
+    if (this.quality <= 0) return (this.quality = 0);
 
     this.sellIn < 0 ? (this.quality -= 2) : this.quality--;
   }
